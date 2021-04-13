0feaccc3368a7c3230421e2ece14fdb7c6cd08bf
vallebz <80699091+vallebz@users.noreply.github.com>
2021-04-13 14:21:21 +0200

    Delete roguelikeCity_transparent.tsx

--- roguelikeCity_transparent.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="roguelikeCity_transparent" tilewidth="16" tileheight="16" spacing="1" tilecount="1036" columns="37">
- <image source="roguelikeCity_transparent.png" trans="ff00ff" width="628" height="475"/>
-</tileset>
